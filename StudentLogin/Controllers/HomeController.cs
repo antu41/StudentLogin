3b413a974c9b60b083c7e6f148bb3116f595ffe4
Antu Das <Antu Das@DESKTOP-8RJKEQ7>
2024-01-07 17:38:09 +0600

    Add project files.

---
+++ StudentLogin/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentLogin.Models;
+using System.Diagnostics;
+
+namespace StudentLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MyDBContext context;
+
+        public HomeController(MyDBContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                return RedirectToAction("Profile");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Std user)
+        {
+            var myUser = context.Stds.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault();
+            if (myUser != null)
+            {
+                HttpContext.Session.SetString("UserSession",myUser.Email);
+                return RedirectToAction("Profile");
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed..";
+            }
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(Std user)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.Stds.AddAsync(user);
+                await context.SaveChangesAsync();
+                TempData["Succees"] = "Registered Successfully";
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StudentLogin/Models/MyDBContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StudentLogin.Models
+{
+    public partial class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+        }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Std> Stds { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Std>(entity =>
+            {
+                entity.ToTable("Std");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Department)
+                    .HasMaxLength(10)
+                    .HasColumnName("department")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("date")
+                    .HasColumnName("dob");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(10)
+                    .HasColumnName("gender")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Phone)
+                    .HasColumnType("numeric(18, 0)")
+                    .HasColumnName("phone");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
