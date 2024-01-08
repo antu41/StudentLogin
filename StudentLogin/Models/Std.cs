eaa625604fb50f04313abfb053a03d72aee6d4cd
antu41 <93597785+antu41@users.noreply.github.com>
2024-01-08 16:40:27 +0600

    Update Std.cs

--- StudentLogin/Models/Std.cs
+++ StudentLogin/Models/Std.cs
@@ -1,17 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudentLogin.Models
 {
     public partial class Std
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = null!;
+        [Required]
         public string Gender { get; set; } = null!;
+        [Required]  
+        [DataType(DataType.Date)]
         public DateTime Dob { get; set; }
+        [Required]
         public string Email { get; set; } = null!;
+        [Required]
         public decimal Phone { get; set; }
+        [Required]
         public string Department { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
     }
 }
